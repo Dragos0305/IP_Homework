2e1d24733621b9bb577c53a48f638b7c48690b0d
veginho <raulvegakinder@gmail.com>
2022-10-22 17:11:21 +0300

    Adaugare functionalitate add si delete element din coada

--- queue.java
+++ queue.java
@@ -15,4 +15,39 @@ class Queue
         rear = -1;
         count = 0;
     }
+    public int dequeue()
+    {
+        // check for queue underflow
+        if (isEmpty())
+        {
+            System.out.println("Underflow\nProgram Terminated");
+            System.exit(-1);
+        }
+ 
+        int x = arr[front];
+ 
+        System.out.println("Removing " + x);
+ 
+        front = (front + 1) % capacity;
+        count--;
+ 
+        return x;
+    }
+ 
+    // Utility function to add an item to the queue
+    public void enqueue(int item)
+    {
+        // check for queue overflow
+        if (isFull())
+        {
+            System.out.println("Overflow\nProgram Terminated");
+            System.exit(-1);
+        }
+ 
+        System.out.println("Inserting " + item);
+ 
+        rear = (rear + 1) % capacity;
+        arr[rear] = item;
+        count++;
+    }
 }
