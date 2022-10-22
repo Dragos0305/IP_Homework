bb08f1a452e96d94654bdd45d98c5926c11caa57
Dragos0305 <stratulatdragos2000@gmail.com>
2022-10-22 07:10:11 -0700

    Add push function
    Signed-off-by: Dragos0305 <stratulatdragos2000@gmail.com>

--- stack.java
+++ stack.java
@@ -15,4 +15,23 @@ class Stack {
     capacity = size;
     top = -1;
   }
+
+  // push elements to the top of stack
+  public void push(int x) {
+    if (isFull()) {
+      System.out.println("Stack OverFlow");
+
+      // terminates the program
+      System.exit(1);
+    }
+
+    // insert element on top of stack
+    System.out.println("Inserting " + x);
+    arr[++top] = x;
+  }
+
+  // check if the stack is full
+  public Boolean isFull() {
+    return top == capacity - 1;
+  }
 }
